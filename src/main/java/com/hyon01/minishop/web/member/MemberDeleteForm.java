d660de8bb58009a49ec48d8cf435a9cbeb97503e
HyonHyon <khm3626@gmail.com>
2023-11-05 02:18:42 +0900

    fixed js,validation bug

--- src/main/java/com/hyon01/minishop/web/member/MemberDeleteForm.java
+++ src/main/java/com/hyon01/minishop/web/member/MemberDeleteForm.java
@@ -3,7 +3,6 @@ package com.hyon01.minishop.web.member;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 @Data
 public class MemberDeleteForm {
